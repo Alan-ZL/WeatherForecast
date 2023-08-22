acb6b9e16c25a4c84915abfbe609066ef27b7acd
Alan <gtrade1xxii@gmail.com>
2023-08-22 19:27:02 +0800

    tailwind init

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -10,6 +10,9 @@ const count = ref(0)
   <h1>{{ msg }}</h1>
 
   <div class="card">
+    <h1 class="text-3xl font-bold underline">
+      Hello world!
+    </h1>
     <button type="button" @click="count++">count is {{ count }}</button>
     <p>
       Edit
@@ -19,9 +22,8 @@ const count = ref(0)
 
   <p>
     Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
+    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank">create-vue</a>, the official Vue + Vite
+    starter
   </p>
   <p>
     Install
